7143410d508e09294ba6f1bcab59385a8b2131ab
Sasith Warnaka <sasith.w@softcodeit.com>
2023-06-16 18:54:50 +0530

    feat : db connection mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://new-user-31:IZPA7MVgxdArPKWz@cluster0.745b4y5.mongodb.net/nestjs?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
