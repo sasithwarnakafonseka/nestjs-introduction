5f6f400fb2c6674d27f35b2584e563db2ddfd04a
Sasith Warnaka <sasith.w@softcodeit.com>
2023-06-17 12:20:35 +0530

    feat : typeorm setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
+import { ProductsModule } from './api/products/products.module';
+import { getEnvPath } from './common/helper/env.helper';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
+import { ApiModule } from './api/api.module';
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule, 
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+    ApiModule,],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
